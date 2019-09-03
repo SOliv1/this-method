14a14b065be931b8e6f23a784f1976423551518c
Ubuntu <ubuntu@ip-172-31-18-22.ec2.internal>
2019-09-03 15:55:31 +0000

    red and black boxes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-$('button').mouseenter(function() {
-	$(this).removeClass('makeRed').addClass('makeBlue');
-});
+$(document).ready(function() {
+    $(".box").on("click", function() {
+    	/**
+    	 * When we click on an element that has
+    	 * a 'box' class, this event will be fired.
+    	 */
+    	var classNames = $(this).attr("class").split(" ");
+        $("." + classNames[1]).css("background-color", "red");
 
-$('button').mouseleave(function() {
-	$(this).removeClass('makeBlue').addClass('makeRed');
+    });
+
 });
